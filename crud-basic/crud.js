de7325eb4222ce58e0abe18ff8b82dec2fe4e292
SandovalJr <edubizzle7@gmail.com>
2022-05-04 21:53:21 -0500

    ya imprime los resultados

--- crud-basic/crud.js
+++ crud-basic/crud.js
@@ -1,4 +1,5 @@
 let arraydata = [];
+let contadordatatable = 0;
 
 function senddata() {
   // console.log('funciona');
@@ -18,5 +19,34 @@ function senddata() {
   };
 
   arraydata.push(jsondata);
-//   console.log(arraydata);
+  // console.log(arraydata);
+
+  datatable();
+}
+
+function datatable() {
+  for (let i = contadordatatable; i < arraydata.length; i++) {
+    // console.log(`for contador ${i}`);
+    let filaActual = document.getElementById("tblDatos").insertRow(i);
+
+    for (let j = 0; j < 5; j++) {
+      var city = filaActual.insertCell(j);
+      var edad = filaActual.insertCell(j);
+      var email = filaActual.insertCell(j);
+      var name = filaActual.insertCell(j);
+      var id = filaActual.insertCell(j);
+    }
+
+    id.innerHTML = arraydata[i].id;
+    name.innerHTML = arraydata[i].name;
+    email.innerHTML = arraydata[i].email;
+    edad.innerHTML = arraydata[i].edad;
+    city.innerHTML = arraydata[i].city;
+  }
+
+  contadordatatable++;
+}
+
+function eliminar(id) {
+  arraydata.splice(id, 1);
 }
